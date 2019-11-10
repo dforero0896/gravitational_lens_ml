3553668acceda4dbaf2ec91e3c4df20fb9bd8b34
Daniel Felipe Forero Sanchez <df.forero10@gmail.com>
2019-11-10 22:09:48 +0100

    Fix get data

--- src/get_dataset.sh
+++ src/get_dataset.sh
@@ -7,9 +7,16 @@ echo Getting train data into $DATA
 if [[ ! -e $DATA/datapack2.0train.tar.gz ]];then
 wget -P $DATA http://metcalf1.difa.unibo.it/DATA3/datapack2.0train.tar.gz
 fi
-tar xvzf $DATA/datapack2.0train.tar.gz -C $DATA
+if [[ ! -e $DATA/datapack2.0train ]]; then
+mkdir -v $DATA/datapack2.0train
+fi
+tar xvzf $DATA/datapack2.0train.tar.gz -C $DATA/datapack2.0train
 echo Getting test data into $DATA
 if [[ ! -e $DATA/datapack2.0test.tar.gz ]];then
 wget -P $DATA http://metcalf1.difa.unibo.it/DATA3/datapack2.0test.tar.gz
 fi
-tar xvzf $DATA/datapack2.0test.tar.gz -C $DATA
+if [[ ! -e $DATA/datapack2.0test ]]; then
+mkdir -v $DATA/datapack2.0test
+fi
+tar xvzf $DATA/datapack2.0test.tar.gz -C $DATA/datapack2.0test
+wget -P $DATA/catalog/ http://metcalf1.difa.unibo.it/DATA3/image_catalog2.0train.csv
