d869920a207d854043d3a43a258d3075a772113c
Daniel Forero <daniel.forerosanchez@epfl.ch>
2019-12-07 14:57:26 +0100

    Config file works on lastro_cnn

--- src/lastro_v1.py
+++ src/lastro_v1.py
@@ -3,11 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import sys
 import pickle
-if len(sys.argv) != 3:
-        sys.exit('ERROR:\tPlease provide the path of the project directory.\nUSAGE:\t%s USE_GPU PROJECT_DIR\n'%sys.argv[0])
-if not bool(int(sys.argv[1])):
-    sys.stdout.write('Not using GPU.\n')
-    os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+import configparser
 import tensorflow as tf
 from data_generator_function import TiffImageDataGenerator
 import numpy as np
@@ -19,15 +15,36 @@ import re
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, BatchNormalization
+from sklearn.metrics import roc_curve 
 from tensorflow import keras
 from helpers import build_generator_dataframe, get_file_id
-WORKDIR=os.path.abspath(sys.argv[2])
+
+if len(sys.argv) != 2:
+    config_file = 'config.ini'
+else:
+    config_file = sys.argv[1]
+if not os.path.isfile(config_file):
+    sys.exit('ERROR:\tThe config file %s was not found.'%config_file)
+
+config = configparser.ConfigParser()
+config.read(config_file)
+
+print("\nConfiguration file:\n")
+for section in config.sections():
+    print("Section: %s" % section)
+    for options in config.options(section):
+        print("  %s = %s" % (options, config.get(section, options)))
+if not bool(config['general'].getboolean('use_gpu')):
+    sys.stdout.write('\nNot using GPU.\n')
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+        
+###### Paths
+WORKDIR = config['general']['workdir']    
 sys.stdout.write('Project directory: %s\n'%WORKDIR)
 SRC = os.path.join(WORKDIR, 'src')
-DATA = os.path.join(WORKDIR,'data')
+DATA = os.path.join(WORKDIR, 'data')
 RESULTS = os.path.join(WORKDIR, 'results')
-TRAIN_MULTIBAND = os.path.join(DATA, 'train_multiband')
-TRAIN_MULTIBAND_AUGMENT = os.path.join(DATA, 'train_multiband_augment')
+TRAIN_MULTIBAND = config['general']['train_multiband']
 TEST_MULTIBAND = os.path.join(DATA, 'test_multiband')
 
 lens_df = pd.read_csv(os.path.join(RESULTS, 'lens_id_labels.csv'), index_col = 0)
@@ -37,16 +54,21 @@ n_lens_clean = len(lens_df[lens_df['is_lens'] == True])
 n_nolens_clean = len(lens_df[lens_df['is_lens'] == False])
 equal_class_coeff = np.array([n_lens_clean/n_nolens_clean,1])
 natural_class_coeff = np.array([1000 * n_lens_clean/n_nolens_clean,1])
-
-batch_size = 32 
-epochs = 10
+# Training parameters
+batch_size = config['trainparams'].getint('batch_size')
+epochs = config['trainparams'].getint('epochs')
 IMG_HEIGHT = 200
 IMG_WIDTH = 200
-data_bias = 'raw'
+data_bias = config['trainparams']['data_bias']
+test_fraction = config['trainparams'].getfloat('test_fraction')
+augment_train_data = bool(int(config['trainparams']['augment_train_data']))
 
-train_df, val_df = train_test_split(dataframe_for_generator, test_size=0.6, random_state=42)
+train_df, val_df = train_test_split(dataframe_for_generator, test_size=test_fraction, random_state=42)
 total_train = len(train_df)
 total_val = len(val_df)
+print("The number of objects in the whole training sample is: ", total_train)
+print("The number of objects in the whole validation sample is: ", total_val)
+print("The test fraction is: ", test_fraction)
 image_data_gen_train = TiffImageDataGenerator(featurewise_center=False,
                                           samplewise_center=False,
                                           featurewise_std_normalization=False,
@@ -70,21 +92,50 @@ image_data_gen_train = TiffImageDataGenerator(featurewise_center=False,
                                           validation_split=0.2,
                                           dtype='float32')
 image_data_gen_val = TiffImageDataGenerator(dtype='float32')
+bands = [config['bands'].getboolean('VIS0'), 
+    config['bands'].getboolean('NIR1'),
+    config['bands'].getboolean('NIR2'),
+    config['bands'].getboolean('NIR3')]
+print("The bands are: ", bands)
 
+ratio = float(config['trainparams']['lens_nolens_ratio'])
 train_data_gen = image_data_gen_train.prop_image_generator_dataframe(train_df,
                                   directory=TRAIN_MULTIBAND,
                                   x_col='filenames',
                                   y_col='labels', 
 				  batch_size = batch_size, 
-				  validation=True,
-				  ratio=0.5,
-				  bands = [True, False, False, False])
+				  validation=not(augment_train_data),
+				  ratio=ratio,
+				  bands = bands, binary = False)
 val_data_gen = image_data_gen_val.prop_image_generator_dataframe(val_df,
                                   directory=TRAIN_MULTIBAND,
                                   x_col='filenames',
-                                 y_col='labels', batch_size = batch_size, validation=True, ratio = 0.5)
+                                  y_col='labels', 
+				  batch_size = batch_size, 
+				  validation=True, 
+				  ratio = ratio, bands = bands, 
+				  binary = False)
+roc_val_data_gen = image_data_gen_val.prop_image_generator_dataframe(val_df,
+                                  directory=TRAIN_MULTIBAND,
+                                  x_col='filenames',
+                                  y_col='labels', 
+                                  batch_size=batch_size, 
+				  validation=True, 
+				  ratio=ratio,
+                                  bands=bands,
+				  binary=False)
 
+temp_data_gen = image_data_gen_train.image_generator_dataframe(train_df,
+                                  directory=TRAIN_MULTIBAND,
+                                  x_col='filenames',
+                                  y_col='labels', 
+				  batch_size=batch_size, 
+				  validation=True,
+                                  bands=bands,
+				  binary=False)
 
+image, _ = next(temp_data_gen)
+input_shape = image[0].shape
 # Define correct bias to initialize
 output_bias = tf.keras.initializers.Constant(np.log(n_lens_clean/n_nolens_clean))
 
@@ -99,7 +150,7 @@ metrics = [keras.metrics.TruePositives(name='tp'),
 
 model = Sequential([
     Conv2D(16, 4, padding='same', activation='relu', 
-           input_shape=(IMG_HEIGHT, IMG_WIDTH ,1)),
+           input_shape=input_shape),
     Conv2D(16, 3, padding='same', activation='relu'),
     MaxPooling2D(pool_size=(2,2)),
     BatchNormalization(axis=-1),
@@ -113,15 +164,15 @@ model = Sequential([
     BatchNormalization(axis=-1),
     Dropout(0.2),
     Conv2D(128, 3, padding='same', activation='relu'),
-    Dropout(0.1),
+    Dropout(0.2),
     Conv2D(128, 3, padding='same', activation='relu'),
     BatchNormalization(axis=-1),
-    Dropout(0.05),
+    Dropout(0.2),
     Flatten(),
     Dense(1024, activation='relu'),
-    Dropout(0.025),
+    Dropout(0.2),
     Dense(1024, activation='relu'),
-    Dropout(0.0125),
+    Dropout(0.2),
     Dense(1024, activation='relu'),
     BatchNormalization(axis=-1),
     Dense(1, activation='sigmoid')
@@ -132,12 +183,42 @@ model.compile(optimizer='adam',
               metrics=metrics)
 model.summary()
 
-checkpoint_path = os.path.join(RESULTS, 'checkpoints/lastro_cnn/lastro_cnn.ckpt')
-checkpoint_dir = os.path.dirname(checkpoint_path)
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
+save_dir = os.path.join(RESULTS, 'checkpoints/lastro_cnn/')
+model_type = 'lastro_cnn'
+model_name = '%s_Tr%i_Te%i_bs%i_ep%.03d_aug%i_VIS%i_NIR%i%i%i_DB%s_ratio%.01f.h5' % (model_type,
+                                                                        total_train,
+                                                                        total_val,
+                                                                        batch_size,
+                                                                        epochs,
+                                                                        int(augment_train_data),
+                                                                        bands[0], 
+                                                                        bands[1],
+                                                                        bands[2],
+                                                                        bands[3],
+                                                                        data_bias,
+                                                                        ratio)
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
+print("The model name is: ", model_name)
+checkpoint_dir = os.path.dirname(filepath)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=filepath,
+                                                 save_best_only=True,
+                                                 verbose=1,
+						 monitor='val_acc')
 
+es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_acc', 
+					       min_delta=1, 
+					       patience=10, 
+					       verbose=0, 
+				               mode='auto', 
+					       baseline=None, 
+					       restore_best_weights=True)
+lr_reducer = tf.keras.callbacks.ReduceLROnPlateau(factor=np.sqrt(0.1),
+		                                  cooldown=0,
+                  		                  patience=5,
+		                                  min_lr=0.5e-6,
+						  monitor='val_acc')
 # Define class weights for unevenly distributed (biased) dataset.
 if data_bias == 'natural':
     sys.stdout.write('Using natural data bias: 1000x more non lenses than lenses.\n')
@@ -153,20 +234,30 @@ else:
 class_weights = {0:class_coeff[0], 1:class_coeff[1]}
 sys.stdout.write('Using weights: %s\n'%class_weights)
 
-es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=1, patience=5, verbose=0, mode='auto', baseline=None, restore_best_weights=True)
 history = model.fit_generator(
     train_data_gen,
     steps_per_epoch=total_train//batch_size,
     epochs=epochs,
     validation_data=val_data_gen,
-    validation_steps=total_train//batch_size,
-    callbacks = [cp_callback, es_callback],
-    class_weight = class_weights
+    validation_steps=total_val//batch_size,
+    callbacks = [cp_callback, es_callback, lr_reducer],
+    class_weight = class_weights,
+    use_multiprocessing=True,
+    verbose=2
 )
 
-model.save(os.path.join(RESULTS,'lastro_cnn.h5'))
-with open(os.path.join(RESULTS,'lastro_cnn.history'), 'wb') as file_pi:
+model.save(os.path.join(RESULTS,model_name))
+with open(os.path.join(RESULTS,model_name.replace('h5', 'history')), 'wb') as file_pi:
         pickle.dump(history.history, file_pi)
+# Score trained model.
+scores = model.evaluate_generator(val_data_gen, verbose=2, steps=total_val//batch_size)
+images_val, labels_true = next(roc_val_data_gen)
+labels_score = model.predict(images_val, batch_size=subsample_val, verbose=2)
+fpr, tpr, thresholds = roc_curve(np.ravel(labels_true), np.ravel(labels_score))
+print(fpr)
+print(tpr)
 
 
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
 
