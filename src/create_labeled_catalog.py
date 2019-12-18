452c2aed7f0b0663ff7c83eea224cb6b4855a51a
Daniel Forero <daniel.forerosanchez@epfl.ch>
2019-12-18 23:59:58 +0100

    fix

--- src/create_labeled_catalog.py
+++ src/create_labeled_catalog.py
@@ -28,12 +28,10 @@ missing_img = np.setdiff1d(image_catalog.ID.values,
 # http://metcalf1.difa.unibo.it/DATA3/evaluation.pdf
 #image_catalog['is_lens'] = (image_catalog['mag_lens'] > 1.2) & (
 #    image_catalog['n_sources'] != 0)
-image_catalog['nothing']=0
-image_catalog['nothing'][(image_catalog['mag_eff'] > 1.2) & (
-    image_catalog['mag_eff'] < 2)] = np.nan
-image_catalog = image_catalog.dropna(subset=['nothing', 'mag_eff'])
 image_catalog['is_lens'] = (image_catalog['mag_eff'] > 2)
-image_catalog['is_lens'][(image_catalog['mag_eff']<1.2) | (image_catalog['n_sources']==0)] = False
+image_catalog['is_non_lens'] = (image_catalog['mag_eff']<1.2) | (image_catalog['n_sources']==0)
+image_catalog['not_none'] = image_catalog['is_lens'] | image_catalog['is_non_lens']
+image_catalog = image_catalog[image_catalog['not_none']]
 # Add a flag to lines with no corresponding image
 image_catalog['img_exists'] = True
 image_catalog['img_exists'].loc[image_catalog['ID'].isin(missing_img)] = False
@@ -41,6 +39,7 @@ image_catalog['img_exists'].loc[image_catalog['ID'].isin(missing_img)] = False
 image_catalog = image_catalog.drop_duplicates('ID')
 print('Number of lenses: %i' % image_catalog['is_lens'].sum())
 print('Number of non lenses: %i' % (~image_catalog['is_lens']).sum())
+print('Total lines remaining: %i'%len(image_catalog[image_catalog['img_exists']]))
 # Save a new catalog with just ID, is_lens.
 image_catalog[['ID',
                'is_lens']][image_catalog['img_exists']].astype(int).to_csv(os.path.join(
